7d492e39bb65d12a01617e110b744a6a2c3c0f30
Osama Soliman <solimanware@gmail.com>
2023-05-19 04:45:10 +0300

    new method of displaying pdf

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
+//Your Google Drive Link for Resume
+const docLink = 'https://docs.google.com/document/d/1d0zBLP8jK8cZZ7JJSNQUPyZcDRf2LUpbE1o9bLHm1sw'
+const iframe = document.querySelector("iframe");
+iframe.src = `https://docs.google.com/gview?url=${docLink}/export?format=pdf&embedded=true`;
 document.querySelectorAll(".download-btn").forEach((el) => {
   el.addEventListener("click", (e) => {
-    download("./Osama_Soliman_Resume.pdf", "Osama_Soliman_Resume.pdf");
+    download(`${docLink}/export?format=pdf`);
   });
 });
 
-function download(url, filename) {
-  fetch(url).then(function (t) {
-    return t.blob().then((b) => {
-      var a = document.createElement("a");
-      a.href = URL.createObjectURL(b);
-      a.setAttribute("download", filename);
-      a.click();
-    });
-  });
+function download(url) {
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "resume.pdf";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 }
