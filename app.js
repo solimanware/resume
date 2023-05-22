e8dd3fa90b57fd50a006ed013d946dcb1e21bfcf
Osama Soliman <solimanware@gmail.com>
2023-05-22 05:43:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ document.querySelectorAll(".download-btn").forEach((el) => {
 function download(url) {
   const a = document.createElement("a");
   a.href = url;
-  a.download = "resume.pdf";
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
